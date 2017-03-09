7e823fc7a11a67426ddb0e261f8de3c5781c6127
Romain32190 <romain.lafargue36@gmail.com>
2017-03-09 16:53:34 +0100

    Modification fichier app.js

---
+++ Java/app.js
@@ -0,0 +1,12 @@
+var solution = Math.floor(Math.random() * 4);
+console.log(solution);
+
+var response = prompt("Veuiller entrer un nombre entre 0 et 3!");
+console.log(response);
+
+if response==solution(1, 2, 3)){
+	console.log(1, 2, 3);
+
+}
+
+		
