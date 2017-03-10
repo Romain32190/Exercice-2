5c6b7388e5d67fb8310ed1f59378a5e3f346a31f
Romain32190 <romain.lafargue36@gmail.com>
2017-03-10 13:33:19 +0100

    Ajout else if et if

--- Java/app.js
+++ Java/app.js
@@ -2,20 +2,30 @@ var solution = Math.floor(Math.random() * 4);
 console.log(solution);
 
 for (var i = 0; i < 3 ; i++) {
-var response = prompt("Veuiller entrer un nombre entre 0 et 3!");
-console.log(response);
+	var response = prompt("Veuiller entrer un nombre entre 0 et 3!");
+	console.log(response);
 
-if (response==solution) {
-	console.log("Félicitation tu as gagné bravo!");
-	break;
-}
+	if (response==solution) {
+		console.log("Félicitation tu as gagné bravo!");
+		break;
+	}
 
-else{
-console.log("Tu as perdu");
-}
+	else if (response < solution){
+	console.log("C'est trop petit");
+    
+    }
 
-}
+	else {
+	console.log("C'est trop grand");
 
+	}
+
+	if (i == 2){;
+	console.log("C'est perdu")
+
+	}
+
+    }
 
 
 
